90d6f5039d4be75ff301bfe3291674afdc302e5d
par-default <tbilger24@gmail.com>
2022-05-17 20:16:19 +0200

    link entre les crud

--- projet_fh_django/fh/forms.py
+++ projet_fh_django/fh/forms.py
@@ -7,19 +7,22 @@ from django import forms
 class FactionForm(ModelForm):
     class Meta:
         model = models.Faction
-        fields = ('Nom','faction_image')
+        fields = ('Nom','faction_image',)
         labels = {
             'Nom':'',
             'faction_image':'',
+
         }
 
 class HeroForm(ModelForm):
     class Meta:
         model = models.Hero
-        fields = ('hero_img', 'Nom', 'Type', 'Difficulte')
+        fields = ('hero_img', 'Nom', 'Type', 'Difficulte','faction')
         labels = {
-            'hero_img':'',
-            'Nom':'',
-            'Type':'',
-            'Difficulte':'',
+            'hero_img': 'Ajouter une image',
+            'Nom': 'Nom du hero',
+            'Type': 'Type du hero',
+            'Difficulte': 'Difficulté du hero',
+            'Faction': 'Faction du hero',
+
         }
--- projet_fh_django/fh/models.py
+++ projet_fh_django/fh/models.py
@@ -18,8 +18,9 @@ class Faction(models.Model):  # déclare la classe Livre héritant de la classe 
 
     Nom = models.CharField(max_length=100)  # défini un champs de type texte de 100caractères maximum
     faction_image = models.ImageField(null=True, blank=True, upload_to="images/")
+
     def dico(self):
-        return {"Nom":self.Nom, "faction_image":self.faction_image}
+        return {"Nom":self.Nom, "faction_image":self.faction_image, "hero":self.hero}
 
 
 class Hero(models.Model):
@@ -27,6 +28,6 @@ class Hero(models.Model):
     Nom = models.CharField(max_length=100)
     Type = models.CharField(max_length=10, choices=TYPE, default= 'facile')
     Difficulte = models.CharField(max_length=15, choices=DIFF, default='gardien')
-
+    faction = models.ForeignKey("faction", on_delete=models.CASCADE, default=None)
     def dico(self):
         return {"hero_img":self.hero_img, "Nom":self.Nom, "Type":self.Type, "Difficulte":self.Difficulte}
